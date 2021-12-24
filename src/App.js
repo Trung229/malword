d67d16d9e48e454c2bf94fa8186b203eb6824ea3
Trung <ductrung4263@gmail.com>
2021-12-24 16:31:10 +0700

    add title text

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import Layout from './Layouts/index'
 import UpdatePage from './Page/UpdatePage'
 
-const arr = []
 
 function App() {
-  for(let i = 0; i < 100; i++){
-    arr.push(i)
-  }
   return (
     <Layout>
+      <h1>Trung</h1>
       <UpdatePage />
     </Layout>
   );
